0d0d5ecb5aa80b6f8fa5c75dd5213e6c5fead120
Mike Chalmers <mikechalmers@gmail.com>
2020-06-27 15:59:39 +0100

    added post routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+/*jshint esversion: 6 */
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
---
+++ routes/posts.js
@@ -0,0 +1,40 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const router = express.Router();
+
+/* GET posts index /posts */
+router.get('/', (req, res, next) => {
+  res.send('this is the /posts page');
+});
+
+/* GET new post /posts/new */
+router.get('/new', (req, res, next) => {
+  res.send('this is the /posts/new page');
+});
+
+/* POST new post /posts */
+router.post('/', (req, res, next) => {
+  res.send('this is the POST /posts page');
+});
+
+/* GET SHOW page /posts/:id */
+router.get('/:id', (req, res, next) => {
+  res.send('this is the /posts/ SHOW page for IDs');
+});
+
+/* GET edit post /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) => {
+  res.send('this is to edit the /posts/:id content');
+});
+
+/* PUT update /posts/:id */
+router.put('/:id', (req, res, next) => {
+  res.send('this is to update the /posts/:id content');
+});
+
+/* DESTROY the /posts/:id */
+router.delete('/:id', (req, res, next) => {
+  res.send('this is to destroy the /posts/:id');
+});
+
+module.exports = router;
