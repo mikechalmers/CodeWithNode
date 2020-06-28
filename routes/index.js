1b4160188a918b0a98d907a8241e0f1d6c3537fe
Mike Chalmers <mikechalmers@gmail.com>
2020-06-28 14:33:49 +0100

    refactor user routes into index routes

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,55 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+/* GET /register user. */
+router.get('/register', (req, res, next) => {
+  res.send("this be the register user form");
+});
+
+/* POST /register user. */
+router.post('/register', (req, res, next) => {
+  res.send("this be the POST register user form");
+});
+
+/* GET /login user. */
+router.get('/login', (req, res, next) => {
+  res.send("this be the register GET login form");
+});
+
+/* POST /login user. */
+router.post('/login', (req, res, next) => {
+  res.send("this be the POST login user form");
+});
+
+/* GET user profile. */
+router.get('/profile', (req, res, next) => {
+  res.send("this be the GET user profile");
+});
+
+/* PUT update user profile/:user_id . */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send("PUT the /profile/:user_id");
+});
+
+/* GET forgot password. */
+router.get('/forgot', (req, res, next) => {
+  res.send("this be the GET forgot password");
+});
+
+/* GET forgot password. */
+router.put('/forgot', (req, res, next) => {
+  res.send("this be the PUT forgot password");
+});
+
+/* GET reset password. */
+router.get('/reset/:token', (req, res, next) => {
+  res.send("this be the GET reset password with token");
+});
+
+/* PUT reset password. */
+router.put('/reset/:token', (req, res, next) => {
+  res.send("this be the PUT reset password with token");
+});
+
+
 module.exports = router;
