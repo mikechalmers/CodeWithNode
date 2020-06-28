08d88305cefd4368d0f8433020b93777dd9221c4
Mike Chalmers <mikechalmers@gmail.com>
2020-06-28 15:18:37 +0100

    Setup passport

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 /*jshint esversion: 6 */
+
+// require packages
 const createError   = require('http-errors');
 const express       = require('express');
 const path          = require('path');
 const cookieParser  = require('cookie-parser');
 const logger        = require('morgan');
+const bodyParser    = require('body-parser');
+const passport      = require('passport');
+const session       = require('express-session');
 
+// require user - passportLocalMongoose code
+const User = require('.models/user');
+
+// require routes
 const indexRouter     = require('./routes/index');
 const postsRouter     = require('./routes/posts');
 const reviewsRouter   = require('./routes/reviews');
@@ -21,6 +30,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// configure ExpressSessions - has to be before passport
+
+// configure Passport - passportLocalMongoose code
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser()); // first part comes from passport, second part (passed in) comes from passportLocalMongoose
+passport.deserializeUser(User.deserializeUser());
+
+// mount routes
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
