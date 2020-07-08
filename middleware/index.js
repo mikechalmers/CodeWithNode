6506d9c43e94818631f3a9e430179aa44611a95c
Mike Chalmers <mikechalmers@gmail.com>
2020-07-08 20:03:44 +0100

    Add auth to review edits

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,26 @@
 /*jshint esversion: 8 */
 
+const Review = require('../models/review');
+
 module.exports = {
+
   asyncErrorHandler: (fn) =>
     (req, res, next) => {
       Promise.resolve(fn(req, res, next))
               .catch(next);
+  },
+
+  isReviewAuthor: async (req, res, next) => {
+    // we ID the review first
+    let review = await Review.findById(req.params.review_id);
+    // mognoose gives us the equals helper method - allowing us to check object ID against object ID as === doesn't work
+    if (review.author.equals(req.user._id)) {
+      return next();
+    }
+    // no else needed due to return statement above
+    // error and redirect
+    req.session.error = 'You\'re not allowed to edit comments by other users.';
+    return res.redirect('/');
   }
+
 };
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,10 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const router = express.Router({ mergeParams: true }); //merge params allows me to look up the post i.e. the :id of the overall post from the URL
-const { asyncErrorHandler } = require('../middleware');
+const {
+  asyncErrorHandler,
+  isReviewAuthor
+ } = require('../middleware');
 const {
     reviewCreate,
     reviewUpdate,
@@ -12,7 +15,7 @@ const {
 router.post('/', asyncErrorHandler(reviewCreate));
 
 /* PUT update /posts/:id/reviews/:review_id */
-router.put('/:review_id', asyncErrorHandler(reviewUpdate));
+router.put('/:review_id', isReviewAuthor, asyncErrorHandler(reviewUpdate));
 
 /* DESTROY the /posts/:id/reviews/:review_id */
 router.delete('/:review_id', (req, res, next) => {
