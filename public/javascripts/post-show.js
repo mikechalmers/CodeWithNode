859f8f856c64ae6e0a458f986dd54c2ee2a67b91
Mike Chalmers <mikechalmers@gmail.com>
2020-07-09 12:36:52 +0100

    Fix review edit and map display functionality

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -4,45 +4,54 @@
 // Initialise Map
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlrZWNoYWxtZXJzIiwiYSI6ImNrYzI5dTNoZTIxanMyd214cHJpZmZpdW4ifQ.e3vhqufW1d5uUry3RG8lYg';
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mikechalmers/ckc4vs39n13g61iqgatv4ryoa',
-  center: post.coordinates,
-  zoom: 10
-});
+if (Array.isArray(post.coordinates) && post.coordinates.length) {
 
-// // Load GeoJSON data
-//
-// var geojson = {
-//   type: 'FeatureCollection',
-//   features: [{
-//     type: 'Feature',
-//     geometry: {
-//       type: 'Point',
-//       coordinates: [151.21111, -33.88611]
-//     },
-//     properties: {
-//       title: 'Mapbox',
-//       description: 'Surry Hills !!!'
-//     }
-//   }]
-// };
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mikechalmers/ckc4vs39n13g61iqgatv4ryoa',
+    center: post.coordinates,
+    zoom: 10
+  });
 
-// Add marker to map
 
-// create a HTML element
-var el = document.createElement('div');
-el.className = 'marker';
+  // // Load GeoJSON data
+  //
+  // var geojson = {
+  //   type: 'FeatureCollection',
+  //   features: [{
+  //     type: 'Feature',
+  //     geometry: {
+  //       type: 'Point',
+  //       coordinates: [151.21111, -33.88611]
+  //     },
+  //     properties: {
+  //       title: 'Mapbox',
+  //       description: 'Surry Hills !!!'
+  //     }
+  //   }]
+  // };
 
-// make a marker and add to the map
-new mapboxgl.Marker(el)
-  .setLngLat(post.coordinates)
-  .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups - pixels above marker center
-  .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
-  .addTo(map);
+  // Add marker to map
+
+  // create a HTML element
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  // make a marker and add to the map
+  new mapboxgl.Marker(el)
+    .setLngLat(post.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups - pixels above marker center
+    .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
+    .addTo(map);
+
+
+} // end of if
+
+
 
 
 // toggle edit review form - my own ES6 js
+
   button = document.querySelectorAll('.toggle-edit-form');
   for (i = 0; i < button.length; i++) {
     button[i].onclick = function() {
